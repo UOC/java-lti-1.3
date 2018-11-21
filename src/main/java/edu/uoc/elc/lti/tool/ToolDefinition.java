1579af981bf0bd902a26afa5dba550ac48396b47
Xavi Aracil <xaracil@uoc.edu>
2018-11-21 16:18:55 +0100

    Added missing class

---
+++ src/main/java/edu/uoc/elc/lti/tool/ToolDefinition.java
@@ -0,0 +1,18 @@
+package edu.uoc.elc.lti.tool;
+
+import lombok.Getter;
+import lombok.experimental.Builder;
+
+/**
+ * @author Xavi Aracil <xaracil@uoc.edu>
+ */
+@Getter
+@Builder
+public class ToolDefinition {
+	private String clientId;
+	private String name;
+	private String keySetUrl;
+	private String accessTokenUrl;
+	private String privateKey;
+	private String publicKey;
+}
