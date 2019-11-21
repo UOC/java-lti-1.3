b1ddca99ca8f2d2f78993094d4f3657a85aaa679
Xavi Aracil <xaracil@uoc.edu>
2019-11-21 15:48:46 +0100

    More time to test tokens

--- src/test/java/edu/uoc/elc/lti/tool/claims/TestJWSClaimAccessor.java
+++ src/test/java/edu/uoc/elc/lti/tool/claims/TestJWSClaimAccessor.java
@@ -6,15 +6,15 @@ import java.util.Date;
  * @author Xavi Aracil <xaracil@uoc.edu>
  */
 public class TestJWSClaimAccessor extends JWSClaimAccessor {
-	private final static long _1_YEAR = 365 * 24 * 60 * 60;
+	private final static long _5_YEAR = 5 * 365 * 24 * 60 * 60;
 
 	public TestJWSClaimAccessor(String keySetUrl) {
 		super(keySetUrl);
-		allowedClockSkewSeconds = _1_YEAR;
+		allowedClockSkewSeconds = _5_YEAR;
 	}
 
 	@Override
 	public Date getExpiration() {
-		return new Date(System.currentTimeMillis() + _1_YEAR);
+		return new Date(System.currentTimeMillis() + _5_YEAR);
 	}
 }
