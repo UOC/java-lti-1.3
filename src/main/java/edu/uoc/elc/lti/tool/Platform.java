1de2931f3fcedfec9979cb957200562cc636e3de
heutelbeck <dheutelbeck@ftk.de>
2020-02-26 14:47:16 +0100

    fix compatibility issue with Moodle 3.8

--- src/main/java/edu/uoc/elc/lti/tool/Platform.java
+++ src/main/java/edu/uoc/elc/lti/tool/Platform.java
@@ -1,5 +1,7 @@
 package edu.uoc.elc.lti.tool;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,6 +10,15 @@ import lombok.Setter;
  */
 @Getter
 @Setter
+/*
+ * To fix an issue with Moodle 3.8 not compliant with LTI. While LTI 1.3
+ * specifies the key to be "product_family_code", Moodle 3.8 uses
+ * "familiy_code". The @JsonIgnoreProperties annotation ignores the bad key for
+ * now without remapping it to the original spec.
+ * 
+ * @author heutelbeck
+ */
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Platform {
 	private String guid;
 	private String name;
